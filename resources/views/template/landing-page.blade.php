17ca1cadbf1c19b5de2cd0e707ce20c7ebb45fd9
Menno Tempelaar <info@mentod.nl>
2022-04-13 16:22:23 +0200

    feat(mobile-menu): added bullets

--- resources/views/template/landing-page.blade.php
+++ resources/views/template/landing-page.blade.php
@@ -110,6 +110,56 @@
                     </a>
                 </div>
             </div>
+            <div class="w-2/3 float-right grid grid-cols-4 pr-8">
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+                <div class="relative w-[54px] h-[54px]">
+                    <figure
+                        class="absolute bottom-0 right-0 block rounded-full bg-primary w-1 h-1"></figure>
+                </div>
+            </div>
         </mobile-menu>
 
         <main class="grow">
